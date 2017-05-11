6b5d6cef354b1cee0c1504a6776b3c10dabd6542
David Ahern <david.ahern@emc.com>
2017-05-11 08:16:30 +0100

    Added return types

--- lib/quicksort/custom-quicksort.ts
+++ lib/quicksort/custom-quicksort.ts
@@ -1,17 +1,17 @@
-export function customQuicksort(arr, customSort: Function) {
+export function customQuicksort<T>(arr, customSort: Function): T[] {
 
-  if (!arr || arr.length === 0) {
-    return [];
-  }
+    if (!arr || arr.length === 0) {
+        return [];
+    }
 
-  let left = [];
-  let right = [];
-  let pivot = arr[0];
+    let left = [];
+    let right = [];
+    let pivot = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    customSort(pivot, arr[i]) ? left.push(arr[i]) : right.push(arr[i]);
-  }
+    for (let i = 1; i < arr.length; i++) {
+        customSort(pivot, arr[i]) ? left.push(arr[i]) : right.push(arr[i]);
+    }
 
-  return [...customQuicksort(left, customSort), ...pivot, ...customQuicksort(right, customSort)];
+    return [...customQuicksort(left, customSort), ...pivot, ...customQuicksort(right, customSort)];
 
 }
--- lib/quicksort/quicksort.ts
+++ lib/quicksort/quicksort.ts
@@ -1,16 +1,16 @@
-export function quicksort(arr) {
+export function quicksort(arr): number[] {
 
-  if (!arr || arr.length === 0) {
-    return [];
-  }
+    if (!arr || arr.length === 0) {
+        return [];
+    }
 
-  let left = [];
-  let right = [];
-  let pivot = arr[0];
+    let left = [];
+    let right = [];
+    let pivot = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
-  }
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+    }
 
-  return [...quicksort(left), ...pivot, ...quicksort(right)];
+    return [...quicksort(left), ...pivot, ...quicksort(right)];
 }
