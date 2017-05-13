461db7db7d41161f0d1530caa94e4025c235abd8
ahernd2 <david.ahern26@gmail.com>
2017-05-13 19:44:23 +0100

    Added backwards selection sort

--- lib/sort/insertion/insertion-sort-desc.ts
+++ lib/sort/insertion/insertion-sort-desc.ts
@@ -1,11 +1,10 @@
-export function insertionSort<T>(arr: T[]): T[] {
+export function insertionSortDesc<T>(arr: T[]): T[] {
 
     for (let i = 1; i < arr.length; i++) {
-
         const key = arr[i];
         let j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] < key) {
             arr[j + 1] = arr[j];
             j--;
         }
